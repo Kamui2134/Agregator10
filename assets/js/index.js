c0e06979105c89267f858a1cb0e2924c99b851ab
Шичков Александр <ctorkey@gmail.com>
2024-12-05 18:56:56 +0300

    header is almost done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,72 @@
-"use strict";
+'use strict'
 
 // FAQ
-const questions = document.querySelectorAll('.faq__question');
-const questionsBtns = document.querySelectorAll('.faq__question-btn');
+const questions = document.querySelectorAll('.faq__question')
+const questionsBtns = document.querySelectorAll('.faq__question-btn')
 
 // Устанавливаем первоначальные maxHeight для всех вопросов
 function setInitialMaxHeight() {
-	questions.forEach((question) => {
-		const headHeight = question.querySelector('.faq__question-head').clientHeight;
-		question.style.maxHeight = headHeight + 40 + 'px';
-	});
+	questions.forEach(question => {
+		const headHeight = question.querySelector(
+			'.faq__question-head'
+		).clientHeight
+		question.style.maxHeight = headHeight + 40 + 'px'
+	})
 }
 
-setInitialMaxHeight();
+setInitialMaxHeight()
 
 // Обработчик изменения размера окна
 window.addEventListener('resize', () => {
 	questions.forEach((question, index) => {
-		const isOpen = questionsBtns[index].style.transform === 'rotate(90deg)';
-		const headHeight = question.querySelector('.faq__question-head').clientHeight;
-		question.style.maxHeight = isOpen ? question.scrollHeight + 'px' : headHeight + 40 + 'px';
-	});
-});
+		const isOpen = questionsBtns[index].style.transform === 'rotate(90deg)'
+		const headHeight = question.querySelector(
+			'.faq__question-head'
+		).clientHeight
+		question.style.maxHeight = isOpen
+			? question.scrollHeight + 'px'
+			: headHeight + 40 + 'px'
+	})
+})
 
 // Функция для переключения видимости вопроса
 function questionToggle(index) {
-	const question = questions[index];
-	const btn = questionsBtns[index];
-	const headHeight = question.querySelector('.faq__question-head').clientHeight;
+	const question = questions[index]
+	const btn = questionsBtns[index]
+	const headHeight = question.querySelector('.faq__question-head').clientHeight
 
-	const isOpen = btn.style.transform === 'rotate(90deg)';
+	const isOpen = btn.style.transform === 'rotate(90deg)'
 
-	btn.style.transform = isOpen ? 'rotate(0deg)' : 'rotate(90deg)';
-	question.style.maxHeight = isOpen ? headHeight + 40 + 'px' : question.scrollHeight + 'px';
+	btn.style.transform = isOpen ? 'rotate(0deg)' : 'rotate(90deg)'
+	question.style.maxHeight = isOpen
+		? headHeight + 40 + 'px'
+		: question.scrollHeight + 'px'
 }
 
 // Добавляем обработчики событий на кнопки
 questionsBtns.forEach((btn, i) => {
-	btn.addEventListener('click', () => questionToggle(i));
-});
+	btn.addEventListener('click', () => questionToggle(i))
+})
+
+// HEADER-BTN
+
+const headerBtn = document.querySelector('.header__btn')
+const navLinks = document.querySelectorAll('.header__nav-link')
+
+navLinks.forEach((link, index) => {
+	link.style.setProperty('--index', 80 * index + 'ms')
+})
+
+headerBtn.addEventListener('click', function () {
+	this.classList.toggle('active')
+	navLinks.forEach(link => {
+		link.classList.toggle('active')
+	})
+})
+navLinks.forEach(link => {
+	link.addEventListener('click', function () {
+		navLinks.forEach(link => {
+			link.classList.remove('active')
+		})
+	})
+})
