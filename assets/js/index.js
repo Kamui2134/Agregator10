0a28ab53ccbd1967ce494acc431ce895cc2188ac
Шичков Александр <ctorkey@gmail.com>
2024-12-13 19:00:13 +0300

    mini commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -81,3 +81,66 @@ async function shareSite () {
 		url: url.href,
 	})
 }
+
+// GAME-RATING
+const gameRating = null 
+if (document.querySelector('.game-rating')) {
+	const gameRatings = document.querySelectorAll('.game-rating')
+	gameRatings.forEach((gameRating) => {
+		const rating = parseInt(gameRating.dataset.rating)
+		if (rating > 6) {
+			rating = 5
+		}
+		for (let i = 0; i < rating; i++) {
+			gameRating.children[i].classList.add('rated')
+		}
+	})
+} 
+
+// BEST GAME IN GAMES
+function toggleBestGames() {
+	if (window.innerWidth <= 540 && wasAdded === false) {
+		const bestGames = document.querySelector('.best-games')
+		const games = document.querySelector('.games__games')
+		const bestCasino = games.firstElementChild.cloneNode(true)
+		const bestGameLogo = bestGames.querySelector('.best-games__logo')
+		const bestGameTitle = bestGames.querySelector('.best-games__game-title')
+		const bestGameBonus = bestGames.querySelector('.best-games__game-bonus')
+		const bestGameAdvantages = bestGames.querySelectorAll(
+			'.best-games__advantage-text'
+		)
+
+		const bestCasinoLogoClone = bestCasino.querySelector('.games__game-logo')
+		bestCasinoLogoClone.src = bestGameLogo.src
+		bestCasinoLogoClone.alt = bestGameLogo.alt
+		bestCasino.querySelector('.games__game-name').textContent =
+			bestGameTitle.textContent
+		bestCasino.querySelector('.games__bonus-text').textContent =
+			bestGameBonus.textContent
+		games.insertBefore(bestCasino, games.firstChild)
+		wasAdded = true
+	} else if (window.innerWidth > 540 && wasAdded === true) {
+		const games = document.querySelector('.games__games')
+		games.firstChild.remove()
+		wasAdded = false
+		while (gamesContainer.children.length > allCasinosCount) {
+			gamesContainer.removeChild(gamesContainer.lastChild)
+		}
+	}
+}
+let allCasinosCount = null
+let wasAdded = false
+if (document.querySelector('.games__games')) {
+	allCasinosCount = document.querySelector('.games__games').children.length
+	toggleBestGames()
+
+	window.addEventListener('resize', () => {
+		if (document.querySelector('.companies')) {
+			scrollerInner.innerHTML = ''
+			addCompanies()
+			addAnimation()
+		}
+		toggleBestGames()
+	})
+}
+
